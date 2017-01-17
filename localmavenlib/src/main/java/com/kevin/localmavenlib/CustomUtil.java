c4989167dced480cc9ed5c668f059ade501d426d
KevinLee <kevinlee.vivian@gmail.com>
2017-01-17 10:38:53 +0800

    第一次提交

---
+++ localmavenlib/src/main/java/com/kevin/localmavenlib/CustomUtil.java
@@ -0,0 +1,21 @@
+package com.kevin.localmavenlib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * ClassName:
+ * Description:
+ * Author:KevinLee
+ * Date:2017/1/16 0016
+ * Time:下午 6:13
+ * Email:KevinLeeV@163.com
+ */
+
+public class CustomUtil {
+
+    public static void show(Context context){
+        Toast.makeText(context, "LearnMaven", Toast.LENGTH_SHORT).show();
+    }
+
+}
